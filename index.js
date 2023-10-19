3480d96ce2b23d77aa307dfe65999feacdca10b7
jamesh21 <jamesh21@uw.edu>
2023-10-18 23:25:17 -0700

    finished styling desktop view

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
